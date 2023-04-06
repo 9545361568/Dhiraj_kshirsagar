fc30b98eee17036f436ecc386d34d9158280f6fa
statment <dhirajuttareshwarkshirsagar@dhirajs-MacBook-Pro.local>
2023-04-06 11:00:02 +0530

    basic program

---
+++ src/ArithmeticTest.java
@@ -0,0 +1,12 @@
+public class ArithmeticTest {
+    public static void main(String[] java)
+    {
+        int num1= 1001;
+        int num2= 200;
+        System.out.println("addition:"+(num1+num2));
+        System.out.println("sub:"+(num1-num2));
+        System.out.println("square of num2:"+(num1*num2));
+        System.out.println("division:"+(num1/num2));
+        System.out.println("mod:"+(num1%num2));
+    }
+}
